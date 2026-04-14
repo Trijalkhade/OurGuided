dc6d9963e064f51c8e79aca1b1739272a77f8921
Trijal Vijay Khade <165871777+Trijalkhade@users.noreply.github.com>
2026-04-14 19:55:03 +0530

    html pages moved to public

--- frontend/src/pages/PrivacyPolicy.jsx
+++ frontend/src/pages/PrivacyPolicy.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import privacyHtml from './privacy.html?raw';
 
 const PrivacyPolicy = () => {
-    return (
-        <div 
-            className="privacy-policy-container" 
-            style={{ 
-                padding: '2rem', 
-                maxWidth: '1000px', 
-                margin: '0 auto', 
-                backgroundColor: 'white', 
-                color: 'black',
-                lineHeight: '1.6',
-                fontFamily: 'Arial, sans-serif'
-            }}
-        >
-            <div dangerouslySetInnerHTML={{ __html: privacyHtml }} />
-        </div>
-    );
+  return (
+    <iframe
+      src="/privacy.html"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+      title="Privacy Policy"
+    />
+  );
 };
 
 export default PrivacyPolicy;
