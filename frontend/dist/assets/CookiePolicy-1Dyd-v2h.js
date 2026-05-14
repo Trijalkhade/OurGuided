2ede17372d7129215ec31a4c4abc13348ffce667
Ubuntu <ubuntu@ip-172-31-25-95.ec2.internal>
2026-05-14 10:56:51 +0000

    remove dist from tracking

--- frontend/dist/assets/CookiePolicy-1Dyd-v2h.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Q9MPMWHo.js";const t=()=>o.jsx("iframe",{src:"/cookies.html",style:{width:"100%",height:"100vh",border:"none"},title:"Cookie Policy"});export{t as default};
--- frontend/dist/assets/PrivacyPolicy-8H1oztpv.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-Q9MPMWHo.js";const i=()=>r.jsx("iframe",{src:"/privacy.html",style:{width:"100%",height:"100vh",border:"none"},title:"Privacy Policy"});export{i as default};
